adf13f558bd2900e0099d5970aa593d78c22aa67
Vincent Daubry <vdaubry@gmail.com>
2015-10-30 08:24:05 +0100

    improve performance for tape_equilibrium : score 100

--- 1_Time_complexity/tape_equilibrium.rb
+++ 1_Time_complexity/tape_equilibrium.rb
@@ -12,7 +12,6 @@ class Array
   end
 end
 
-
 def first_part(array, partition_index)
   array[0..partition_index-1]
 end
@@ -21,24 +20,24 @@ def second_part(array, partition_index)
   array[partition_index..-1]
 end
 
-def difference(array, partition_index)
-  difference = first_part(array, partition_index).sum - second_part(array, partition_index).sum
-  difference.abs
-end
-
 def minimal_difference(array)
-  result = nil
+  first_part_sum = first_part(array, 1).sum
+  second_part_sum = second_part(array, 1).sum
+  result = (first_part_sum - second_part_sum).abs
+  
   array.each_with_index do |value, index|
-    next if index == array.count - 1
-    current_difference = difference(array, index+1)
-    result = current_difference if result.nil? || current_difference < result
+    next if index < 1 || index == array.count - 1
+    first_part_sum += value
+    second_part_sum -= value
+    current_difference = (first_part_sum - second_part_sum).abs
+    result = current_difference if current_difference < result
   end
   result
 end
 
 def solution(a)
   minimal_difference(a)
-end  
+end
 
 
 array = [3, 1, 2, 4, 3]
@@ -48,11 +47,6 @@ raise "invalid result : #{result}" unless result == [3]
 result = second_part(array, 1)
 raise "invalid result : #{result}" unless result == [1, 2, 4, 3]
 
-result = difference(array, 1)
-raise "invalid result : #{result}" unless result == 7
-
-result = difference(array, 2)
-raise "invalid result : #{result}" unless result == 5
 
 result = minimal_difference(array)
 raise "invalid result : #{result}" unless result == 1
@@ -65,7 +59,7 @@ array = [-1000, 1000]
 result = minimal_difference(array)
 raise "invalid result : #{result}" unless result == 2000
 
-array = [rand(100)]*10000
+array = [rand(100)]*100000
 Timeout::timeout(1) {
   minimal_difference(array)
 }
