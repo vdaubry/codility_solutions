45b2d4ea1d3919cbd3b9c836862c7bc697881a74
Vincent Daubry <vdaubry@gmail.com>
2015-11-05 15:36:05 +0100

    improve performance for genomic_range_query : score 100

--- 3_Prefix_Sums/genomic_range_query.rb
+++ 3_Prefix_Sums/genomic_range_query.rb
@@ -9,17 +9,36 @@ IMPACT_FACTOR = {
 
 def solution(s, p, q)
   array = s.chars
+  size = array.size
+  letters = IMPACT_FACTOR.keys
   
-  p.each_with_index.map do |p_value, p_index|
-    index_start = p_value
-    index_stop = q[p_index]
-    nucleotides = array[index_start..index_stop]
-    min_nucleotide = nucleotides.min_by do |nucleotide|
-      IMPACT_FACTOR[nucleotide]
+  prefix_sums = Hash.new {|h, k| h[k] = Array.new(size, 0) }
+  
+  array.each_with_index do |nucleotide, index|
+    letters.each do |letter|
+      current_val = prefix_sums[letter][index-1]
+      current_val+=1 if letter == nucleotide
+      prefix_sums[letter][index] = current_val
     end
-    
-    IMPACT_FACTOR[min_nucleotide]
   end
+  
+  result = []
+  p.zip(q) do |p_value, q_value|
+    letters.each do |letter|
+      start_index = [p_value-1, 0].max
+      stop_index = q_value
+      
+      if p_value == 0 && array[0] == letter
+        result << IMPACT_FACTOR[array[0]]
+        break
+      elsif prefix_sums[letter][start_index] != prefix_sums[letter][stop_index]
+        result << IMPACT_FACTOR[letter]
+        break
+      end
+    end
+  end
+  
+  result
 end
 
 
@@ -35,10 +54,12 @@ class AlgoTests < MiniTest::Unit::TestCase
   describe "method" do
     it { assert_equal [2, 4, 1], solution("CAGCCTA", [2, 5, 0], [4, 5, 6]) }
     it { assert_equal [1, 2, 1], solution("GCATCGTA", [1, 3, 0], [4, 5, 6]) }
+    it { assert_equal [1, 2, 1], solution("GCATCGTAGCTACGTA", [1, 3, 0], [4, 5, 6]) }
     it { assert_equal [3], solution("G", [0], [0]) }
     it { assert_equal [3, 3], solution("G", [0, 0], [0, 0]) }
     it { assert_equal [4], solution("GT", [1], [1]) }
     it { assert_equal [4, 4], solution("GT", [1, 1], [1, 1]) }
+    it { assert_equal [1, 1, 2], solution("AC", [0, 0, 1], [0, 1, 1]) }
   end
 end
 
@@ -48,16 +69,23 @@ end
 # Benchmark
 #
 ######
-#require 'minitest/benchmark'
-#class PerformanceBench < MiniTest::Benchmark    
-#  def self.bench_range 
-#    Minitest::Benchmark.bench_exp(10, 100_000)
-#  end
-#  
-#  def bench_linear_performance
-#    assert_performance_constant 0.99 do |input|
-#      #generate random dataset, ex: input.times {|val| array << rand(val)}
-#      #call method to test
-#    end
-#  end
-#end
+require 'minitest/benchmark'
+class PerformanceBench < MiniTest::Benchmark    
+  def self.bench_range 
+    Minitest::Benchmark.bench_exp(1, 100_000)
+  end
+ 
+  def bench_linear_performance
+    assert_performance_linear 0.99 do |input|
+      str = ""
+      p = []
+      q = []
+      input.times do |input| 
+        str += ["A","C","G","T"].sample
+        p << input
+        q << input+1
+      end
+      solution(str, p, q)
+  end
+ end
+end
